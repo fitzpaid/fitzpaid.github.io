4a3c240b7a02a93fd01b8760a48708a89953cc80
Ian Fitzpatrick <ian.fitzpatrick6@gmail.com>
2024-01-10 21:12:49 +0000

    add in blog definition in config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
 const gamedev = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
@@ -13,4 +26,4 @@ const gamedev = defineCollection({
 	}),
 });
 
-export const collections = { gamedev };
+export const collections = { gamedev, blog };

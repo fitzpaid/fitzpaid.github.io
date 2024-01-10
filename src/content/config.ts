f32e76b802b2aa9713a19c68dd2f6dbc77fa48e0
Ian Fitzpatrick <ian.fitzpatrick6@gmail.com>
2024-01-10 21:18:05 +0000

    actually let's just remove references to blog cause I've no idea what I'm doing yet

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,5 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-
 const gamedev = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
@@ -26,4 +13,4 @@ const gamedev = defineCollection({
 	}),
 });
 
-export const collections = { gamedev, blog };
+export const collections = { gamedev };
